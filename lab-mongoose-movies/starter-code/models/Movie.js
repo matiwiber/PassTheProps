d154e3531a78368ace18338ef55500517ab1fe13
Mati Weber <mati.weber@gmail.com>
2021-02-08 19:59:07 -0300

    done wothout bonus

---
+++ lab-mongoose-movies/starter-code/models/Movie.js
@@ -0,0 +1,13 @@
+let mongoose = require("mongoose");
+let Schema = mongoose.Schema;
+
+let movieSchema = new Schema(
+  {
+    title: String,
+    genre: String,
+    plot: String,
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Movie", movieSchema);
